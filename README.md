7daa9abb4353cddd2c2ca1345073432ce938b5ae
Thomas Willems <twillems@willtho.com>
2019-10-02 14:50:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Help-Pages
+Help Pages for whitelabel platforms
+
+Each file contains the Article content for a HelpPage.type
---
+++ SiteAdminBusinessOverview.py
@@ -0,0 +1,12 @@
+TITLE_DE = "Deuscher Titel; Einzeilig"
+TITLE_EN = "English Title; single line"
+
+CONTENT_DE = """
+Deutscher Inhalt für diese Inhaltsseite
+Kann Mehrzeilig sein
+"""
+
+CONTENT_EN = """
+English Content.
+Can span multiple lines
+"""
